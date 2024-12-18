b2354a9f587050d5cbe5d756d0b7477499761c49
vivek-chauhaan <vivekchauhan1181@gmail.com>
2024-12-19 01:28:17 +0530

    vivek done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 const Home = () => {
   return (
     <div>
-      <h1 className=" text-center text-xl font-bold">HI Bro Kaise ho</h1>
+      <h1 className=" text-center text-xl font-bold">
+        HI Bro Kaise ho vivek chauhan
+      </h1>
     </div>
   );
 };
